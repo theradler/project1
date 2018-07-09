07a967acbac216ba167e2e20a390247460fb7c92
IMPRIVATAINC\radler <radler@imprivata.com>
2018-07-09 18:29:44 -0400

    fixing models.py

--- models.py
+++ models.py
@@ -4,7 +4,7 @@ from application import db
 from sqlalchemy.dialects.postgresql import UUID
 
 
-class User(db.Model):
+class Users(db.Model):
     UserID = db.Column(UUID(as_uuid=True),unique=True, primary_key=True)
     UserName = db.Column(db.String(80), unique=True, nullable=False)
     Password = db.Column(db.String(64))
