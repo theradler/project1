6a007e81e55d2d527a931635569b503eb449bab3
unknown <radler@imprivata.com>
2018-07-09 19:29:23 -0400

    Adding Login and Registration Logic

--- application.py
+++ application.py
@@ -3,12 +3,17 @@ import os
 from flask import Flask, session, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_session import Session
+from flask_login import LoginManager, current_user, login_user
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
-
+from models import User
+from app.forms import LoginForm, RegisterForm
+from sqlalchemy.dialects.postgresql import UUID
+from UUID import uuid4
 
 app = Flask(__name__)
 db = SQLAlchemy()
+login = LoginManager(app)
 
 
 # Check for environment variable
@@ -18,6 +23,7 @@ if not os.getenv("DATABASE_URL"):
 # Configure session
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+app.config['SECRET_KEY'] = 'passwordkey'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://rfiyeehknxkzti:fcd541969aee3f6c579001a4769f90bb3d27c6e5194c31478217c5010e016a22@ec2-54-227-243-210.compute-1.amazonaws.com:5432/d4dhnjm6dcg7mm'
 Session(app)
 
@@ -32,17 +38,39 @@ def index():
     user_session= False
     return render_template("index.html", user_session=user_session)
 
-@app.route("/register")
+@app.route("/register", methods['GET', 'POST'])
 def register():
-    return render_template("register.html")
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(UserName=form.username.data).first()
+        if user is not None:
+            flash("Username already in use")
+        try:
+            db.execute("Insert into User (UserID, UserName, Password) VALUES (:UserID, :UserName, :Password, :Email)"),
+                                    {"UserID": uuid4(), "UserName": form.username.data, "Password": form.password.data}
+            db.commit()
+            return render_template("/index")
 
-@app.route('/login', methods=['POST'])
+@login.user_loader
+def load_user(id):
+    return User.query.get(UUID(id))
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.form['password'] == 'password' and request.form['username'] == 'admin':
-        session['logged_in'] = True
-    else:
-        flash('wrong password!')
-    return index()
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(UserName=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash("Invalid username or password")
+        login_user(user,remember=form.remember_me.data)
+        return redirect(url_for('index'))
+    return render_template('index.html', title='Sign In'm form=from)
+
+
 
 @app.route("/logout")
 def logout():
--- models.py
+++ models.py
@@ -1,14 +1,23 @@
 import uuid
 
-from application import db
+from application import db, login
+from flask_login import UserMixin
 from sqlalchemy.dialects.postgresql import UUID
 
 
-class Users(db.Model):
+
+class User(UserMixin, db.Model):
     UserID = db.Column(UUID(as_uuid=True),unique=True, primary_key=True)
     UserName = db.Column(db.String(80), unique=True, nullable=False)
     Password = db.Column(db.String(64))
-    Email = db.Column(db.String(120), unique=True, nullable=False)
-
+    
     def __repr__(self):
         return '<User %r>' % self.UserName
+
+class Location(db.Model):
+    locatioID = db.Column(UUID(as_uuid=True),unique=True, primary_key=True)
+    Zipcode = db.Column(db.Integer(), unique=True)
+    City = db.Column(db.String(64),unique=True)
+    State = db.Column(db.String(64))
+    Latitude = db.Column(db.Integer())
+    Longitude = db.Column(db.Integer())
