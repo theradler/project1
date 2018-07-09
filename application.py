4c79b82b57b3109e2ae770367a6cfc32a8498db7
IMPRIVATAINC\radler <radler@imprivata.com>
2018-07-09 16:45:15 -0400

    fixed db stuff

--- application.py
+++ application.py
@@ -1,26 +1,29 @@
 import os
 
-from flask import Flask, session, render_template, request
+from flask import Flask, session, render_template, request, flash
+from flask_sqlalchemy import SQLAlchemy
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
+db = SQLAlchemy()
 
 
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
 
-# Configure session to use filesystem
+# Configure session
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://rfiyeehknxkzti:fcd541969aee3f6c579001a4769f90bb3d27c6e5194c31478217c5010e016a22@ec2-54-227-243-210.compute-1.amazonaws.com:5432/d4dhnjm6dcg7mm'
 Session(app)
 
 # Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
-db = scoped_session(sessionmaker(bind=engine))
-
+#engine = create_engine(os.getenv("DATABASE_URL"))
+#db = scoped_session(sessionmaker(bind=engine))
+db.init_app(app) 
 
 @app.route("/")
 
@@ -31,3 +34,19 @@ def index():
 @app.route("/register")
 def register():
     return render_template("register.html")
+
+@app.route('/login', methods=['POST'])
+def login():
+    if request.form['password'] == 'password' and request.form['username'] == 'admin':
+        session['logged_in'] = True
+    else:
+        flash('wrong password!')
+    return index()
+
+@app.route("/logout")
+def logout():
+    session['logged_in'] = False
+    return index()
+
+if __name__ == "__main__":
+    app.run()
--- models.py
+++ models.py
@@ -1,13 +1,11 @@
 import uuid
 
-from app import db
-
+from application import db
 
 class User(db.Model):
-    UserID = db.Column(UUID, unique=True, primary_key=True, nullable=False)
-    username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
 
     def __repr__(self):
-        return '<User {}>'.format(self.username)
+        return '<User %r>' % self.username
